d11c12ecaca5a9cd185a17579d9aed51a8a0ff7e
Krage56 <ivan.proskuryakov.2001@yandex.ru>
2020-06-02 19:08:52 +0300

    operations devided into classes

--- main.cpp
+++ main.cpp
@@ -1,36 +1,18 @@
-//
-// Created by krage56 on 28.03.2020.
-//
-/*Please write only name of test file without control structures
-If the test located one level lower, please write path like lagrange_tests\test.in 
-*/
 using namespace std;
 #include "Field_Z.h"
 #include "polinominal.h"
-
+const size_t mem_block = 40 + 1;
 int main(int args, char*argv[]){
-    size_t N = 0, p = 0;
-    long a = 0;
-    char file_name[mem_block];
-    cin.getline(file_name, mem_block);
-	string path = file_name;
-	string full_path = argv[0];
-	int index = full_path.find_last_of('\\');
-	std::string pathDir = "";
-	if (index > -1)
-		pathDir = full_path.substr(0, index);
-	full_path = pathDir + '\\' + path;
+    char file_name[40 + 1];
+    cin.getline(file_name, mem_block);/*"../test.txt"*/
     fstream file;
-    file.open(full_path, ios_base::out | ios_base::in);
-	vector<vector<Field_Z>> data;//{x, y}
-    if(!processing(&file, N, p, a, data)){
-        cout << "The program can`t open the file" << endl;
+    file.open(/*argv[1]*/file_name, ios_base::out | ios_base::in);
+    DataGetter data(file);
+    bool f = data.processing();
+    if(!f){
         return 1;
     }
-	ofstream answer;
-	answer.open(pathDir + "\\answer.out");
-	answer << calculation(data, p, a) << "\n";
-	answer.close();
-    file.close();
+    LagrangePolinom result(std::move(data.getData()), data.p, data.a);
+    result.calculation();
     return 0;
 }
